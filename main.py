7c57ac7f18c6ad065f1e9c1e388310c405e1af79
Luqman Kittur <kitturluqman007@gmail.com>
2023-12-01 12:05:04 +0530

    sitemap test

--- main.py
+++ main.py
@@ -1,35 +1,26 @@
 from flask import *
-from flask_sitemapper import Sitemapper
-
-sitemapper = Sitemapper()
 
 app = Flask(__name__)
-sitemapper.init_app(app)
 
-@sitemapper.include(lastmod="09-08-2023")
 @app.route('/')
 def index():
     return render_template("index.html")
 
-@sitemapper.include(lastmod="09-08-2023")
 @app.route('/download')
 def download():
     return render_template("downloads.html")
 
-@sitemapper.include(lastmod="09-08-2023")
 @app.route('/privacy')
 def privacy():
     return render_template("privacy.html")
 
-@sitemapper.include(lastmod="09-08-2023")
 @app.route('/cookie')
 def cookie():
     return render_template("cookie.html")
 
-@app.route("/sitemap.xml")
-def sitemap():
-    return sitemapper.generate()
-
+@main.route('/sitemap.xml')
+def static_from_root():
+    return send_from_directory(app.static_folder, request.path[1:])
 
 if __name__ == "__main__":
     app.run()
